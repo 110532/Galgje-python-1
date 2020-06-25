99f8b558780606d543450ca5d14bd5b1e7f14b80
Danique Brink <110532@st-maartenscollege.nl>
2020-06-25 14:16:21 +0000

    Woord veranderd naar 3 letters ipv. 4 letters. Omdat 4 letters toch wel ingewikkelder werd.

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 
 spelen = input("Beginnen aan galgje ja/nee:")
 
-print ("het woord is:" + " " + "_" + " " + "_" + " " + "_" + " " + "_")
+print ("het woord is:" + " " + "_" + " " + "_" + " " + "_")
 
 while spelen != "nee":
     foute_letters = [] 
     letter_1 = input ("kies een letter: ")
-    if letter_1 == "s" or "u" or "r" or "f":
+    if letter_1 == "s" or "a" or "p":
         if letter_1 == "s":
             print("DEZE LETTER IS GOED")
             print("s" + "_" + " " + "_" + " " + "_")
-        elif letter_1 == "u":
+        elif letter_1 == "a":
             print("DEZE LETTER IS GOED")
-            print ("_" + " " + "u" + "_" + " " + "_")
-        elif letter_1 == "r":
+            print ("_" + " " + "a" + "_" + " " + "_")
+        elif letter_1 == "p":
             print ("DEZE LETTER IS GOED")
-            print("_" + " " + "_" + "r" + " " + "_")
+            print("_" + " " + "_" + "p" + " " + "_")
         else:
             foute_letters.insert(0, letter_1)
             print("helemaal fout")
             print("je foute letters zijn nu:" + " " + str(foute_letters))
+
+
