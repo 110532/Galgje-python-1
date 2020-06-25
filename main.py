c8f9e00ee7d207c94f131578db8a2abc5140ff3d
Danique Brink <110532@st-maartenscollege.nl>
2020-06-25 14:01:18 +0000

    Je kan nu de eerste letters invoeren, alleen als je verder gaat dan 1 of 2 letters klopt het nog niet.

--- main.py
+++ main.py
@@ -3,3 +3,21 @@
 spelen = input("Beginnen aan galgje ja/nee:")
 
 print ("het woord is:" + " " + "_" + " " + "_" + " " + "_" + " " + "_")
+
+while spelen != "nee":
+    foute_letters = [] 
+    letter_1 = input ("kies een letter: ")
+    if letter_1 == "s" or "u" or "r" or "f":
+        if letter_1 == "s":
+            print("DEZE LETTER IS GOED")
+            print("s" + "_" + " " + "_" + " " + "_")
+        elif letter_1 == "u":
+            print("DEZE LETTER IS GOED")
+            print ("_" + " " + "u" + "_" + " " + "_")
+        elif letter_1 == "r":
+            print ("DEZE LETTER IS GOED")
+            print("_" + " " + "_" + "r" + " " + "_")
+        else:
+            foute_letters.insert(0, letter_1)
+            print("helemaal fout")
+            print("je foute letters zijn nu:" + " " + str(foute_letters))
