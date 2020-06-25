76df5392f8b7af3dd5be288bd0d63e31291b2e9c
Danique Brink <110532@st-maartenscollege.nl>
2020-06-25 15:37:45 +0000

    Alles weggehaald omdat ik het anders ga doen.

--- main.py
+++ main.py
@@ -2,24 +2,3 @@
 
 spelen = input("Beginnen aan galgje ja/nee:")
 
-print ("het woord is:" + " " + "_" + " " + "_" + " " + "_")
-
-while spelen != "nee":
-    foute_letters = [] 
-    letter_1 = input ("kies een letter: ")
-    if letter_1 == "s" or "a" or "p":
-        if letter_1 == "s":
-            print("DEZE LETTER IS GOED")
-            print("s" + "_" + " " + "_" + " " + "_")
-        elif letter_1 == "a":
-            print("DEZE LETTER IS GOED")
-            print ("_" + " " + "a" + "_" + " " + "_")
-        elif letter_1 == "p":
-            print ("DEZE LETTER IS GOED")
-            print("_" + " " + "_" + "p" + " " + "_")
-        else:
-            foute_letters.insert(0, letter_1)
-            print("helemaal fout")
-            print("je foute letters zijn nu:" + " " + str(foute_letters))
-
-
