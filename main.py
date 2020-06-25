24092bc5db5ce415a49d61a599f3609bd84c5593
Danique Brink <110532@st-maartenscollege.nl>
2020-06-25 20:37:41 +0000

    begin van galgje

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 
 spelen = input("Beginnen aan galgje ja/nee:")
 
+hetwoord=["s","a","p"]
+goedeletters=[]
+fouteletters=[]
+
+while len(goedeletters) < len (hetwoord):
+  nieuweletter = input ("kies een letter:")
+  goed=hetwoord.count(nieuweletter)
+  if goed > 0:
+    print("JA, deze letter is goed")
+    goedeletters.append(nieuweletter)
+  else:
+    print("fout...")
+    fouteletters.append(nieuweletter)
+  
+
+  print("foute letters", fouteletters)
+  print("goede letters", goedeletters)
+
+print("einde, het woord was: SAP")
