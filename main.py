0f228134495d73c28d4eabf279147d509e451381
Danique Brink <110532@st-maartenscollege.nl>
2020-06-27 19:41:57 +0000

    Hoeveelheid fouten aangegeven, opgelost wat er gebeurd als er een letter meerdere keren voorkomt, en je kan het spel nog een keer spelen omdat de vraag nu reageert.

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-# woord_1 = "surf"
 import random
 
-
-
-woordenlijst=["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
-
-
-willekeuriggetal= random.randint(0,10)
-
-hetwoord= woordenlijst [willekeuriggetal]
-goedeletters=[]
-fouteletters=[]
-
-print("Welkom bij galgje, je mag 5 fouten maken om het woord te raden, succes.")
-print("het woord dat je kan raden bestaat uit:", len(hetwoord), "letters")
-
 def printtussenstand ():
-  
+  teradenletters=0
   tussenstand=" "
   for letter in hetwoord:
     geraden=goedeletters.count (letter)
@@ -24,25 +9,64 @@ def printtussenstand ():
       tussenstand= tussenstand + letter
     else:
       tussenstand= tussenstand + "_" 
+      teradenletters=teradenletters+1
     tussenstand= tussenstand + " "
   print(tussenstand)
+  return teradenletters
 
-printtussenstand ()
+woordenlijst=["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
 
-while len(goedeletters) < len (hetwoord) and len(fouteletters) < 5:
-  nieuweletter = input ("kies een letter:") 
-  goed=hetwoord.count(nieuweletter)
-  if goed > 0:
-    print("JA, deze letter is goed")
-    goedeletters.append(nieuweletter)
-  else: 
-    print("fout...")
-    fouteletters.append(nieuweletter)
-  printtussenstand()
+spelen="ja"
 
-  print("foute letters", fouteletters)
+while spelen=="ja":
 
-print("EINDE")
+  fouten=0
+
+  willekeuriggetal= random.randint(0,10)
+
+  hetwoord= woordenlijst [willekeuriggetal]
+  #hetwoord="sap"
+  teradenletters=len(hetwoord)  
+
+  goedeletters=[]
+  fouteletters=[]
+
+  print("Welkom bij galgje, je mag 5 fouten maken om het woord te raden, succes.")
+  print("het woord dat je kan raden bestaat uit:", teradenletters, "letters")
 
 
-spelen = input("Begin opnieuw aan galgje ja/nee:")
+
+  printtussenstand ()
+
+
+  while teradenletters > 0 and fouten < 5:
+    nieuweletter = input ("kies een letter:") 
+    goed=hetwoord.count(nieuweletter)
+    if goed > 0:
+      print("JA, deze letter is goed")
+      goedeletters.append(nieuweletter)
+    else: 
+      print("fout...")
+      fouteletters.append(nieuweletter)
+      fouten=fouten+1 
+      print("Je mag nog", 5-fouten, "fouten maken")
+
+    teradenletters=printtussenstand()
+  
+    print("foute letters", fouteletters)
+
+
+  if teradenletters==0:
+    print("gefelciciteerd, je hebt het woord geraden")
+  else:
+    print("jammer, het woord had moeten zijn:", hetwoord)
+  
+  print()
+  spelen = input("Begin opnieuw aan galgje ja/nee:")
+  print()
+  print()
+
+print("Tot ziens...")
+
+#for (goedeletters) print ("gefeliciteerd")
+#for (fouteletters) print ("jammer, verloren")
